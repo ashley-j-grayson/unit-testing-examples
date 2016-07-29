f005c03dfa422547beded6afb94db14bdfaf82af
ashleygrayson-tsm <Ashley.Grayson@travelsupermarket.com>
2016-07-29 10:01:17 +0100

    Code and Tests for getAverage and sumDouble logic

---
+++ src/main/logic/LogicPuzzles2.java
@@ -0,0 +1,40 @@
+package logic;
+
+import farm.Person;
+
+import java.util.List;
+
+/**
+ * Created by ashleygrayson on 29/07/2016.
+ */
+public class LogicPuzzles2 {
+
+    public double getAverageOfNumbers(int[] numbers) {
+
+        double sum = 0;
+        double average = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+
+            sum = sum + numbers[i];
+            average = sum / numbers.length;
+        }
+
+        return average;
+    }
+
+    public int sumDouble(int[] numbers) {
+
+        int sum = 0;
+        int sumDoubled = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+
+            sum = sum + numbers[i];
+            sumDoubled = sum * 2;
+        }
+
+        return sumDoubled;
+    }
+
+}
---
+++ src/test/logic/LogicPuzzles2Test.java
@@ -0,0 +1,37 @@
+package logic;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+/**
+ * Created by ashleygrayson on 29/07/2016.
+ */
+public class LogicPuzzles2Test {
+
+    private LogicPuzzles2 logicPuzzles2;
+
+    @Before
+    public void before() {logicPuzzles2 = new LogicPuzzles2();}
+
+    @Test
+    public void testAverage() {
+
+        int[] someNumbers = {1, 10, 4};
+
+        double averageNumber = logicPuzzles2.getAverageOfNumbers(someNumbers);
+
+        assertThat(averageNumber, equalTo(5.0));
+    }
+
+    @Test
+    public void sumDouble_shouldTotalTheArrayTheDoubleValue() {
+        int[] someNumbers = {1, 10, 4};
+
+        int sumDoubled = logicPuzzles2.sumDouble(someNumbers);
+
+        assertThat(sumDoubled, equalTo(30));
+    }
+
+}
